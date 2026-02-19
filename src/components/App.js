a7551a7e3fe3125ae30909f4bfb4a0cf3583851e
= <shivampathak19972@gmail.com>
2026-02-19 22:05:32 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [value, setValue] = useState("");
+
+  useEffect(
+    function () {
+      if (value) console.log(value);
+    },
+    [value],
+  );
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <form>
+        <input type="text" onChange={handleChange} value={value} />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
